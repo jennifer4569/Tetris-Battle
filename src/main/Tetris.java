b2be1498a767a5ce97cac1b309081823600e0470
Michael Ruvinshteyn <michael.ruvinshteyn@gmail.com>
2020-08-19 19:53:58 -0400

    why does vscode think that 2 spaces is the adequate indentation size when every other file works fine with 4

--- src/main/Tetris.java
+++ src/main/Tetris.java
@@ -6,112 +6,111 @@ import java.net.*;
 
 public class Tetris extends JFrame {
 
-  private JLabel statusBar;
-  protected TetrisClientHandler clientHandler;
+    private JLabel statusBar;
+    protected TetrisClientHandler clientHandler;
 
-  private void initToolBar() {
-    JToolBar toolBar = new JToolBar();
+    private void initToolBar() {
+        JToolBar toolBar = new JToolBar();
 
-    JButton registerButton = new JButton("Register");
-    registerButton.addActionListener((event) -> {
-      JTextField field1 = new JTextField();
-      JTextField field2 = new JTextField();
-      JPanel panel = new JPanel(new GridLayout(0, 1));
-      panel.add(new JLabel("Username:"));
-      panel.add(field1);
-      panel.add(new JLabel("Password:"));
-      panel.add(field2);
-      int result = JOptionPane.showConfirmDialog(null, panel, "Register", JOptionPane.OK_CANCEL_OPTION,
-          JOptionPane.PLAIN_MESSAGE);
-      if (result == JOptionPane.OK_OPTION) {
-        clientHandler.register(field1.getText(), field2.getText());
-      } else {
-        System.out.println("Cancelled");
-      }
-    });
-    toolBar.add(registerButton);
+        JButton registerButton = new JButton("Register");
+        registerButton.addActionListener((event) -> {
+            JTextField field1 = new JTextField();
+            JTextField field2 = new JTextField();
+            JPanel panel = new JPanel(new GridLayout(0, 1));
+            panel.add(new JLabel("Username:"));
+            panel.add(field1);
+            panel.add(new JLabel("Password:"));
+            panel.add(field2);
+            int result = JOptionPane.showConfirmDialog(null, panel, "Register", JOptionPane.OK_CANCEL_OPTION,
+                                                       JOptionPane.PLAIN_MESSAGE);
+            if (result == JOptionPane.OK_OPTION) {
+                clientHandler.register(field1.getText(), field2.getText());
+            } else {
+                System.out.println("Cancelled");
+            }
+        });
+        toolBar.add(registerButton);
 
-    JButton loginButton = new JButton("Login");
-    loginButton.addActionListener((event) -> {
-      JTextField field1 = new JTextField();
-      JTextField field2 = new JTextField();
-      JPanel panel = new JPanel(new GridLayout(0, 1));
-      panel.add(new JLabel("Username:"));
-      panel.add(field1);
-      panel.add(new JLabel("Password:"));
-      panel.add(field2);
-      int result = JOptionPane.showConfirmDialog(null, panel, "Login", JOptionPane.OK_CANCEL_OPTION,
-          JOptionPane.PLAIN_MESSAGE);
-      if (result == JOptionPane.OK_OPTION) {
-        clientHandler.login(field1.getText(), field2.getText());
-      } else {
-        System.out.println("Cancelled");
-      }
-    });
-    toolBar.add(loginButton);
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener((event) -> {
+            JTextField field1 = new JTextField();
+            JTextField field2 = new JTextField();
+            JPanel panel = new JPanel(new GridLayout(0, 1));
+            panel.add(new JLabel("Username:"));
+            panel.add(field1);
+            panel.add(new JLabel("Password:"));
+            panel.add(field2);
+            int result = JOptionPane.showConfirmDialog(null, panel, "Login", JOptionPane.OK_CANCEL_OPTION,
+                                                       JOptionPane.PLAIN_MESSAGE);
+            if (result == JOptionPane.OK_OPTION) {
+                clientHandler.login(field1.getText(), field2.getText());
+            } else {
+                System.out.println("Cancelled");
+            }
+        });
+        toolBar.add(loginButton);
 
-    add(toolBar, BorderLayout.NORTH);
-  }
-
-  public Tetris() {
-
-    try { 
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-    } 
-    catch (Exception e) {
+        add(toolBar, BorderLayout.NORTH);
     }
 
-    try {
-      String server = "67.205.133.16";
-      int port = 8080;
+    public Tetris() {
 
-      Socket socket = new Socket(server, port);
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+        }
 
-      clientHandler = new TetrisClientHandler(socket);
-      Thread t = new Thread(clientHandler);
-      t.start();
+        try {
+            String server = "67.205.133.16";
+            int port = 8080;
 
-    } catch (Exception e) {
-      e.printStackTrace();
+            Socket socket = new Socket(server, port);
+
+            clientHandler = new TetrisClientHandler(socket);
+            Thread t = new Thread(clientHandler);
+            t.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        initToolBar();
+
+        statusBar = new JLabel("0");
+        add(statusBar, BorderLayout.SOUTH);
+
+        JPanel panel = new JPanel();
+
+        Board board = new Board(this, true);
+        panel.add(board);
+        // board.start();
+
+        Board oppBoard = new Board(this, false);
+        oppBoard.setBackground(Color.LIGHT_GRAY);
+        panel.add(oppBoard);
+
+        panel.setLayout(new GridLayout(1, 0));
+
+        add(panel);
+
+        setTitle("My Tetris");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    initToolBar();
+    public JLabel getStatusBar() {
+        return statusBar;
+    }
 
-    statusBar = new JLabel("0");
-    add(statusBar, BorderLayout.SOUTH);
+    public static void main(String[] args) {
 
-    JPanel panel = new JPanel();
-
-    Board board = new Board(this, true);
-    panel.add(board);
-    // board.start();
-
-    Board oppBoard = new Board(this, false);
-    oppBoard.setBackground(Color.LIGHT_GRAY);
-    panel.add(oppBoard);
-
-    panel.setLayout(new GridLayout(1, 0));
-
-    add(panel);
-
-    setTitle("My Tetris");
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-  }
-
-  public JLabel getStatusBar() {
-    return statusBar;
-  }
-
-  public static void main(String[] args) {
-
-    SwingUtilities.invokeLater(new Runnable() {
-      public void run() {
-        Tetris myTetris = new Tetris();
-        myTetris.setSize(750, 750);
-        myTetris.setLocation(0, 0);
-        myTetris.setVisible(true);
-      }
-    });
-  }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Tetris myTetris = new Tetris();
+                myTetris.setSize(750, 750);
+                myTetris.setLocation(0, 0);
+                myTetris.setVisible(true);
+            }
+        });
+    }
 
 }
