728fdbcb6094f010e5ffbf9716f366b672dbdc68
Jennifer Zhang <jennifer.newyork@aol.com>
2020-08-19 16:59:10 -0400

    client test

--- src/main/TetrisClientHandler.java
+++ src/main/TetrisClientHandler.java
@@ -9,14 +9,24 @@ public class TetrisClientHandler implements Runnable{
 
     }
     public void run(){
-        
+
     }
     public static void main(String[] args){
         try{
             String server = "localhost";
             int port = 8080;
 
+            int i = 0;
+
+
+            System.out.println(i);
+            i++;
+
             Socket socket = new Socket(server, port);
+
+            System.out.println(i);
+            i++;
+
 
 
 
@@ -26,12 +36,17 @@ public class TetrisClientHandler implements Runnable{
             Scanner in = new Scanner(inStream);         
             PrintWriter out = new PrintWriter(outStream, true); //autoflush
 
+            System.out.println(i);
+            i++;
             out.println("REGISTER JENNI WORLD");
+            System.out.println(i);
+            i++;
             // TetrisClientHandler clientHandler = new TetrisClientHandler(socket);
             // Thread t = new Thread(clientHandler);
             // t.start();
         }
         catch(Exception e){
+System.out.println("wot");
         }
     }
 }
